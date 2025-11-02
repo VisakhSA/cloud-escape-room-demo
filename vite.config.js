c73602141387a4bff84de74715f9eed41f10c783
Visakh S A <savisakhmonu@gmail.com>
2025-11-02 18:57:43 +0530

    listen all interface1

--- vite.config.js
+++ vite.config.js
@@ -5,20 +5,19 @@ import react from '@vitejs/plugin-react'
 export default defineConfig({
   plugins: [react()],
   server: {
-    host: '0.0.0.0',
+    host: true, // Listen on all local IPs
     port: 3000,
-    allowedHosts: 'all',
+    strictPort: false,
     proxy: {
       '/api': {
-        target: 'http://0.0.0.0:8080',
+        target: 'http://localhost:8080',
         changeOrigin: true,
         secure: false
       }
     }
   },
   preview: {
-    host: '0.0.0.0',
-    port: 8080,
-    allowedHosts: 'all'
+    host: true,
+    port: 8080
   }
 })
